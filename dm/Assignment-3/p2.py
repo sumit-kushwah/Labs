7efe2c000a841f28b1ef01a2eaf716e67b7b88de
sumit-kushwah <sumitkushwah1729@gmail.com>
2021-02-18 19:33:47 +0530

    Assignment 3 done

---
+++ dm/Assignment-3/p2.py
@@ -0,0 +1,19 @@
+from os import write
+import random, string, time, csv
+begin = time.time()
+csvfile = open('output.csv', 'w')
+fieldnames = ['no_of_files', 'time_taken_sec']
+writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writer.writeheader()
+for filenum in range(1, 501):
+    with open("./files/" + str(filenum) + ".txt", 'r+') as f:
+        out = f.read()
+        f.seek(0)
+        f.write(out.upper())
+        f.close()
+    if (filenum % 100 == 0):
+        print(time.time() - begin)
+        writer.writerow({
+            "no_of_files": filenum,
+            "time_taken_sec": time.time() - begin
+        })
